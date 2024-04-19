b9908a531a975894ad4da4d39edbcbace509080d
Sylvain Schunk <sylvain.katet@hotmail.fr>
2024-04-19 22:34:17 +0200

    tabs works !

--- js/tabs.js
+++ js/tabs.js
@@ -1,21 +1,38 @@
 const linksArray = document.querySelectorAll(".js-tabs > a");
 const articlesArray = document.querySelectorAll("[data-tab-content]");
-let j = 0;
+let i = 0;
+
+// linksArray.forEach(function(element){
+
+//     element.addEventListener('click', function(){
+    
+//         for (const link of linksArray) {
+//                 link.classList.remove('active');
+
+//                 for (const article of articlesArray) {
+//                     article.classList.add('hidden');
+//                     if (this.classList.contains("active")) {
+//                         article.classList.remove('hidden');
+//                     }
+//                 }
+//             }
+//         this.classList.add('active');   
+//     })
+// })
 
 linksArray.forEach(function(element){
 
     element.addEventListener('click', function(){
     
         for (const link of linksArray) {
-                link.classList.remove('active');
-
-                for (const article of articlesArray) {
-                    article.classList.add('hidden');
-                    if (this.classList.contains("active")) {
-                        article.classList.remove('hidden');
-                    }
-                }
-            }
+                link.classList.remove('active');  
+        }
+        i = parseInt(element.dataset.tab);
         this.classList.add('active');   
+        
+        for (const article of articlesArray) {
+            article.classList.add('hidden');  
+        }
+        articlesArray[i-1].classList.remove('hidden')
     })
 })
