aa9ad189bf305bfcaceffaad9c057086d6f5a5a6
Sylvain Schunk <sylvain.katet@hotmail.fr>
2024-04-20 19:26:43 +0200

    salad, not completed, and with template

--- js/salad.js
+++ js/salad.js
@@ -5,26 +5,33 @@ const salad = document.querySelector('#salad-final');
 
 
 for (const ingredient of ingredients) {
-    addIngredientToList(ingredient, list);
+    const ingredientsTemplate = document.getElementById("template-ingredients");
+    const ingredientItem = document.importNode(ingredientsTemplate.content, true);
+    ingredientItem.querySelector('.button').innerText = ingredient;
+    list.appendChild(ingredientItem);
 }
 
 const ingredientBtn = document.querySelectorAll(".button");
 
 ingredientBtn.forEach(function(element){
     element.addEventListener('click', function(){
-        //console.log(element);
-        addIngredientToList(this.innerText, salad);
+        const saladTemplate = document.getElementById("template-salad");
+        const saladItem = document.importNode(saladTemplate.content, true);
+        saladItem.querySelector('.button-salad').innerText = this.innerText;
+        salad.appendChild(saladItem);
         ingredientsNumber = document.getElementById('salad-count');
         ingredientsNumber.innerText++;
         
-        /*const saladCompleted = document.querySelectorAll("#salad-final .button");
-        saladCompleted.forEach(function(e){
+        const ingredientBtnSalad = document.querySelectorAll(".button-salad");
+        ingredientBtnSalad.forEach(function(e){
             e.addEventListener('click', function(){
-                console.log(e);
+                console.log(ingredientBtnSalad); 
             })
-        })*/
+        })
     })
 })
+
+
 
 
 
@@ -33,11 +40,11 @@ ingredientBtn.forEach(function(element){
  * @param {string} ingredient - string representing a picture of the ingredient added
  * @param {ul} list - ul in which the indredient is added
  */
-function addIngredientToList(ingredient, list) {
+/*function addIngredientToList(ingredient, list) {
     const newLi = document.createElement('li');
     list.appendChild(newLi);
     const newButton = document.createElement('button');
     newLi.appendChild(newButton);
     newButton.classList.add("button");
     newButton.innerText = ingredient;
-}
+}*/
