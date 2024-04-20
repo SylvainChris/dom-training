64cc750176d029aa231b0943b1340274db3a81b4
Sylvain Schunk <sylvain.katet@hotmail.fr>
2024-04-20 17:54:00 +0200

    salad, not completed, without template

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,43 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+const list = document.querySelector("#salad-ingredients");
+const salad = document.querySelector('#salad-final');
+
+
+for (const ingredient of ingredients) {
+    addIngredientToList(ingredient, list);
+}
+
+const ingredientBtn = document.querySelectorAll(".button");
+
+ingredientBtn.forEach(function(element){
+    element.addEventListener('click', function(){
+        //console.log(element);
+        addIngredientToList(this.innerText, salad);
+        ingredientsNumber = document.getElementById('salad-count');
+        ingredientsNumber.innerText++;
+        
+        /*const saladCompleted = document.querySelectorAll("#salad-final .button");
+        saladCompleted.forEach(function(e){
+            e.addEventListener('click', function(){
+                console.log(e);
+            })
+        })*/
+    })
+})
+
+
+
+/**
+ * Add an ingredient to a list
+ * @param {string} ingredient - string representing a picture of the ingredient added
+ * @param {ul} list - ul in which the indredient is added
+ */
+function addIngredientToList(ingredient, list) {
+    const newLi = document.createElement('li');
+    list.appendChild(newLi);
+    const newButton = document.createElement('button');
+    newLi.appendChild(newButton);
+    newButton.classList.add("button");
+    newButton.innerText = ingredient;
+}
